0040c1d8031b00cdd31976602a183f4905a48af5
John McBride <john.e.mcbride@icloud.com>
2024-03-10 14:56:38 +0000

    push changes to make admin work

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+FORCE_SCRIPT_NAME = "/prod"
